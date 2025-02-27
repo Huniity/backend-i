b13438af6a98e3a9fe208559d1d554f3c7d5e2cb
Huniity <adrien.dejonc@gmail.com>
2025-02-27 23:34:09 +0000

    CHORE: Session_1 [WIP]

---
+++ Session_1/src/session_1/calculator.py
@@ -0,0 +1,24 @@
+
+
+def Sum():
+    a = 1
+    b = 2
+    sumResult = a + b
+    print(f"Result of {a} + {b} = {sumResult}.")
+
+def Substract():
+    a = 1
+    b = 1
+    substractResult = a - b
+    substractResult2 = b - a
+    print(f"Result of {a} - {b} = {substractResult}.")
+    print(f"Result of {b} - {a} = {substractResult}.")
+
+def divide():
+    a = 1
+    b = 2
+    divideResult = a / b
+    divideResult2 = b / a
+    print(f"Result of {a} - {b} = {divideResult}.")
+    print(f"Result of {b} - {a} = {divideResult2}.")
+          
---
+++ Session_1/src/session_1/main.py
@@ -0,0 +1,16 @@
+print("Hello World")
+import requests
+
+
+response = requests.get("http://colormind.io/api")
+print(response.status_code)
+response.json
+
+
+
+
+
+
+print(dir(response))
+print(response.content)
+print(response.encoding)
