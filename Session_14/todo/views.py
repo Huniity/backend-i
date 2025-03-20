e67f203e58791c493abdb1a32f24079b450ca03c
Huniity <adrien.dejonc@gmail.com>
2025-03-20 21:59:42 +0000

    CHORE: Sesh Django 14

---
+++ Session_14/todo/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import ListView
+
+from todo.models import Task
+
+# Create your views here.
+def index(request):
+    tasks = Task.objects.all()
+    return render(request,"todo/index.html", {"foo":"World", "tasks":tasks})
+
+class IndexView(ListView):
+    model = Task
