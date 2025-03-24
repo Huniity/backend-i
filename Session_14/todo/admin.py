cfd6862edf6addeb2027aa77e3f449648813bca5
Huniity <adrien.dejonc@gmail.com>
2025-03-24 22:05:26 +0000

    CHORE: SESH 14-15-16

--- Session_14/todo/admin.py
+++ Session_14/todo/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 # Register your models here.
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ("title", "description", "due_date", "is_done")
+    list_display = ("title", "description", "due_date", "is_done", "user")
     list_editable = ("due_date", "is_done")
     sortable_by = ("due_date", "is_done", "title")
 
